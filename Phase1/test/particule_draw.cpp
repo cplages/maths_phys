79da06a8582238a4d32b1a66931c3bdc5afd685f
Camille <cplages@enseirb-matmeca.fr>
2018-09-12 02:17:06 +0200

    Particule draw en cours

--- Phase1/test/particule_draw.cpp
+++ Phase1/test/particule_draw.cpp
@@ -12,15 +12,13 @@ int WINDOW_POSITION_Y = 100;
 
 int CAMERA_EYE_X = 70;
 int CAMERA_EYE_Y = -20;
-int CAMERA_EYE_Z = 100;
+int CAMERA_EYE_Z = 250;
 int CAMERA_VIEW_X = 70;
 int CAMERA_VIEW_Y = -20;
 int CAMERA_VIEW_Z = 0;
 int CAMERA_UP_X = 0;
 int CAMERA_UP_Y = 1;
 int CAMERA_UP_Z = 0;
-
-
 
 
 Particule current;
@@ -42,41 +40,41 @@ void display(void)
      GL_COLOR_BUFFER_BIT |
      GL_DEPTH_BUFFER_BIT
      );
-  
-  
+
+
   Vector3D p = current.get_position();
-  /* if(p.get_x() >= (CAMERA_EYE_X *2)) {
+
+  if(p.get_x() >= 200 && launch) {
     stop = true;
-    printf("STOP!\n");
+
   }
-  else {*/
-    glTranslated(p.get_x() - old_p.get_x(), p.get_y() - old_p.get_y(), p.get_z() - old_p.get_z());
-  //glVertex3f(p.get_x(), p.get_y(), p.get_z());
+
+//    glTranslated(p.get_x() - old_p.get_x(), p.get_y() - old_p.get_y(), p.get_z() - old_p.get_z());
+    //glVertex3f(p.get_x(), p.get_y(), p.get_z());
     glutSolidSphere(1,30,30);
 
-    // }
   old_p = p;
   glutSwapBuffers();
   glutPostRedisplay();
 }
 
 void idle_func(void) {
-  if(time < animation_time && launch) {
+  if(time < animation_time && !stop && launch) {
     //give to integer the time between two frame
     current.integer(interval);
     time += interval;
-    current.display();
-    printf("time: %f \n", time);
+    //current.display();
+    //printf("time: %f \n", time);
     //multiply by 1000 due to sleep function taking millisecond in argument.
     usleep(interval*1000);
   }
-  else if( (time >= animation_time /*|| stop*/) && launch) {
-    //stop = false;
+  else if( (time >= animation_time || stop) && launch) {
+    stop = false;
     launch = false;
     time = 0.0f;
     old_p = Vector3D();
     glPopMatrix();
-    printf("go out!\n");
+    printf("End of animation !\n");
   }
   glutPostRedisplay();
 }
@@ -90,22 +88,23 @@ void reshape(int width, int height)
 		 45,
 		 float(width)/float(height),
 		 0.1,
-		 100
+		 250
 		 );
   glMatrixMode(GL_MODELVIEW);
+  printf("RESHAAAAAPE\n" );
 }
 
 void init_particules() {
   int size = 4;
-  Particule gun_bullet = Particule(Vector3D(), Vector3D(35,0,0), 2, 1, 0.70);
-  Particule fire_ball = Particule(Vector3D(), Vector3D(50,0,0), 2, -1, 0.70);
-  Particule paper_ball = Particule(Vector3D(), Vector3D(5,0,0), 2, 0.1, 0.70);
-  Particule canon = Particule(Vector3D(), Vector3D(70,0,0), 2, 6, 0.70);
+  Particule gun_bullet = Particule(Vector3D(), Vector3D(35,0,0), 2, 1, 0.7);
+  Particule fire_ball = Particule(Vector3D(), Vector3D(50,0,0), 2, -1, 0.7);
+  Particule laser = Particule(Vector3D(), Vector3D(100,0,0), 2, 0, 0.7);
+  Particule canon = Particule(Vector3D(), Vector3D(70,0,0), 2, 6, 0.7);
 
   container = new Particule[size]();
   container[0] = gun_bullet;
   container[1] = fire_ball;
-  container[2] = paper_ball;
+  container[2] = laser;
   container[3] = canon;
 
   current = *(container);
@@ -117,6 +116,7 @@ void handler_event(unsigned char key, int x, int y) {
     case '1':
       current = container[0];
       glColor3f(1.0,1.0,1.0);
+      glTranslated(220, 0, 0);
       break;
     case '2':
       current = container[1];
@@ -131,6 +131,7 @@ void handler_event(unsigned char key, int x, int y) {
       glColor3f(0.0,0.0,1.0);
       break;
     case 'l':
+      printf("Particule launched ! \n");
       launch = true;
       glPushMatrix();
       break;
@@ -142,6 +143,13 @@ void handler_event(unsigned char key, int x, int y) {
 }
 
 
+void display_init(){
+  printf("Welcome to the Particule Launcher !\n");
+  printf("You have 4 particules availables : gun bullet, fire ball, laser and canon. Push 1 to 4 respectively to choose one.\n");
+  printf("Then push `l` to launch the particule.\n");
+  printf("When the animation is over, you can start again by choosing a particule and launching it.\n");
+}
+
 int main(int argc, char** argv)
 {
   time = 0.0f;
@@ -149,18 +157,18 @@ int main(int argc, char** argv)
 
   init_particules();
 
+  display_init();
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE);
   glutInitWindowSize(WINDOW_SIZE, WINDOW_SIZE);
   glutInitWindowPosition(WINDOW_POSITION_X, WINDOW_POSITION_Y);
   glutCreateWindow("particule launcher");
-  gluLookAt(CAMERA_EYE_X, CAMERA_EYE_Y, CAMERA_EYE_Z, CAMERA_VIEW_X, CAMERA_VIEW_Y, CAMERA_VIEW_Z, CAMERA_UP_X, CAMERA_UP_Y, CAMERA_UP_Z);  
   glutKeyboardFunc(handler_event);
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
   glutIdleFunc(idle_func);
-  //    InitGL();
-  
+  gluLookAt(CAMERA_EYE_X, CAMERA_EYE_Y, CAMERA_EYE_Z, CAMERA_VIEW_X, CAMERA_VIEW_Y, CAMERA_VIEW_Z, CAMERA_UP_X, CAMERA_UP_Y, CAMERA_UP_Z);
+
   glutMainLoop();
   return 0;
 }
